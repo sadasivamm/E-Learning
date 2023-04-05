79f4b313ac7a331db2268cfad4b853a30b78ed56
sadasivamm <109524194+sadasivamm@users.noreply.github.com>
2023-04-05 01:01:42 +0100

    Add files via upload

---
+++ application/views/backend/admin/blog_add.php
@@ -0,0 +1,18 @@
+<div class="row ">
+    <div class="col-xl-12">
+        <div class="card">
+            <div class="card-body py-2">
+                <h4 class="page-title"> <i class="mdi mdi-apple-keyboard-command title_icon"></i> Short Video
+                </h4>
+            </div> <!-- end card body-->
+        </div> <!-- end card -->
+    </div><!-- end col-->
+</div>
+
+
+<div class="row ">
+    <div class="container" style="height: 1200px; overflow:hidden">
+        
+        <iframe src="<?php echo site_url('shorts/index.php?qa=video'); ?>" height="100%" width="100%" title="" style="border:none" ></iframe>
+    </div>
+</div>
